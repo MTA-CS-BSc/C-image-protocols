ca8a774d79beb37bd850c2f9fff2a358b41b65d5
eyalch <eyalch32@gmail.com>
2022-12-14 19:29:07 +0200

    variables declerations

--- final-c-project/project.c
+++ final-c-project/project.c
@@ -1,1 +1,36 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef unsigned short IMG_POS[2];
+
+typedef struct rgb {
+	unsigned char r, g, b;
+} RGB;
+
+typedef struct tnode_lnode {
+	TNODE* node;
+	struct tnode_lnode* next;
+} TNODE_LNODE;
+
+typedef struct tnode_list {
+	TNODE_LNODE* head;
+	TNODE_LNODE* tail;
+} TNODE_LIST;
+
+typedef struct colorImage {
+	unsigned short rows, cols;
+	RGB** pixels;
+} COLOR_IMAGE;
+
+typedef struct grayImage {
+	unsigned short rows, cols;
+	unsigned char** pixels;
+} GRAY_IMAGE;
+
+typedef struct tnode {
+	IMG_POS position;
+	TNODE_LIST nextPossiblePositions;
+} TNODE;
+
+typedef struct segment {
+	TNODE* root;
+} SEGMENT;
