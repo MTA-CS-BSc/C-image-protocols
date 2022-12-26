fd5ed7d9a1d0a76b7498405ef358253690545454
Maya Raskin <mayaraskin14@gmail.com>
2022-12-26 12:23:49 +0200

    Changed read function to iterate through columns in the rows loop instead of a sepearate loop

--- final-c-project/Q2.h
+++ final-c-project/Q2.h
@@ -2,8 +2,15 @@
 #define __Q2_H
 #include "utils.h"
 
+// Receives a file name that contains PGM data.
+// Creates and returns the gray image read from the file.
 GRAY_IMAGE* readPGM(char*);
+
+// Receives a file, and refs to rows, cols and depth.
+// The function reads the header data from the file and updates the parameters' data.
 void readHeaderFromPGM(FILE*, int*, int*, int*);
+
+// Receives a file and a gray image ref, inserts the data from the file to the pixels array.
 void readDataFromPGM(FILE*, GRAY_IMAGE*);
 
 #endif
