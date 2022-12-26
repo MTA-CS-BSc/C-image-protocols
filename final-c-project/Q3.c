94b05e8a9fd8a94c4c404e68d66b8a3951fc6942
Maya Raskin <mayaraskin14@gmail.com>
2022-12-26 12:21:45 +0200

    Implemented another helper function to keep the code cleaner

--- final-c-project/Q3.c
+++ final-c-project/Q3.c
@@ -1,47 +1,55 @@
 #include "Q3.h"
 
-//unsigned char abs(char number) {
-//	return number >= 0 ? number : -number;
-//}
-#include <math.h>
-
 bool checkIfTresholdOk(unsigned char** pixels, IMG_POS n1, IMG_POS n2, int threshold) {
-	return (pixels[n1[X]][n1[Y]] - pixels[n2[X]][n2[Y]] <= threshold);
+	return (abs(pixels[n1[X]][n1[Y]] - pixels[n2[X]][n2[Y]]) <= threshold);
 }
 
-bool isInRange(GRAY_IMAGE* img, IMG_POS current) {
-	if (current[X] < 0 || current[X] >= img->rows || current[Y] < 0 || current[Y] >= img->cols)
-		return false;
+bool** createPixelsFlagsMatrix(GRAY_IMAGE* img) {
+	bool ** pixels_flags = (bool**)malloc(sizeof(bool*) * img->rows);
 
-	return true;
+	if (!pixels_flags)
+		memoryAllocFailed();
+
+	for (int i = 0; i < img->rows; i++) {
+		pixels_flags[i] = (bool*)malloc(sizeof(bool) * img->cols);
+
+		if (!pixels_flags[i])
+			memoryAllocFailed();
+
+		for (int j = 0; j < img->cols; j++)
+			pixels_flags[i][j] = false;
+	}
+
+	return pixels_flags;
 }
 
-void findSingleSegmentHelper(GRAY_IMAGE* img,
-	IMG_POS start, unsigned char threshold, bool** pixels_flags,
-	TNODE* current_root) {
-
+void findRelevantNeighbors(GRAY_IMAGE* img, TNODE* current_root, IMG_POS start,
+	int threshold, bool** pixels_flags) {
 	IMG_POS current;
-	TNODE_LNODE* current_neighbor;
-
 	int i = current_root->position[X];
 	int j = current_root->position[Y];
 
-	if (img->rows > 0) {
-		for (int x = max(0, i - 1); x <= min(i + 1, img->rows); x++) {
-			for (int y = max(0, j - 1); y <= min(j + 1, img->cols); y++) {
-				if (x != i || y != j) {
-					current[X] = x;
-					current[Y] = y;
+	for (int x = max(0, i - 1); x <= min(i + 1, img->rows); x++) {
+		for (int y = max(0, j - 1); y <= min(j + 1, img->cols); y++) {
+			if (x != i || y != j) {
+				current[X] = x;
+				current[Y] = y;
 
-					if (checkIfTresholdOk(img->pixels, start, current, threshold)
-						&& !pixels_flags[x][y]) {
-						pixels_flags[x][y] = true;
-						insertDataToEndList(&(current_root->nextPossiblePositions), createTNode(current));
-					}
+				if (checkIfTresholdOk(img->pixels, start, current, threshold)
+					&& !pixels_flags[x][y]) {
+					pixels_flags[x][y] = true;
+					insertDataToEndList(&(current_root->nextPossiblePositions), createTNode(current));
 				}
 			}
 		}
 	}
+}
+
+void findSingleSegmentHelper(GRAY_IMAGE* img, IMG_POS start,
+	unsigned char threshold, bool** pixels_flags, TNODE* current_root) {
+	TNODE_LNODE* current_neighbor;
+
+	findRelevantNeighbors(img, current_root, start, threshold, pixels_flags);
 
 	current_neighbor = current_root->nextPossiblePositions.head;
 
@@ -53,27 +61,13 @@ void findSingleSegmentHelper(GRAY_IMAGE* img,
 
 SEGMENT findSingleSegment(GRAY_IMAGE* img,
 	IMG_POS start, unsigned char threshold) {
-	TNODE* root = NULL;
 	SEGMENT segment;
-	bool** pixels_flags = (bool**)malloc(sizeof(bool*) * img->rows);
-	
-	if (!pixels_flags)
-		memoryAllocFailed();
+	bool** pixels_flags = createPixelsFlagsMatrix(img);
+	TNODE* root = createTNode(start);
 
-	root = createTNode(start);
 	makeEmptyTNodeList(&(root->nextPossiblePositions));
-
-	for (int i = 0; i < img->rows; i++) {
-		pixels_flags[i] = (bool*)malloc(sizeof(bool) * img->cols);
-		
-		if (!pixels_flags[i])
-			memoryAllocFailed();
-
-		for (int j = 0; j < img->cols; j++)
-			pixels_flags[i][j] = false;
-	}
-
 	pixels_flags[start[X]][start[Y]] = true;
+
 	findSingleSegmentHelper(img, start, threshold, pixels_flags, root);
 
 	segment.root = root;
--- final-c-project/Q3.h
+++ final-c-project/Q3.h
@@ -1,7 +1,16 @@
 #ifndef __Q3_H
 #define __Q3_H
 #include "utils.h"
-#include <math.h>
+
+bool checkIfTresholdOk(unsigned char** pixels, IMG_POS n1, IMG_POS n2, int threshold);
+
+bool** createPixelsFlagsMatrix(GRAY_IMAGE* img);
+
+void findRelevantNeighbors(GRAY_IMAGE* img, TNODE* current_root, IMG_POS start,
+	int threshold, bool** pixels_flags);
+
+void findSingleSegmentHelper(GRAY_IMAGE* img, IMG_POS start,
+	unsigned char threshold, bool** pixels_flags, TNODE* current_root);
 
 SEGMENT findSingleSegment(GRAY_IMAGE* img,
 	IMG_POS start, unsigned char threshold);
