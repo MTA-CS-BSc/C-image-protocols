b123050e765a16eda0aacf5efcc5ead5aa6ba59a
Maya Raskin <mayaraskin14@gmail.com>
2022-12-26 17:03:18 +0200

    Implemented correct traversal on neighbors

--- final-c-project/Q3.c
+++ final-c-project/Q3.c
@@ -24,26 +24,47 @@ bool** createPixelsFlagsMatrix(GRAY_IMAGE* img) {
 	return pixels_flags;
 }
 
+bool isInRange(int row, int col, GRAY_IMAGE* img) {
+	return (row >= 0 && row < img->rows&& col >= 0 && col < img->cols);
+}
 void findRelevantNeighbors(GRAY_IMAGE* img, TNODE* current_root, IMG_POS start,
 	int threshold, bool** pixels_flags) {
 	IMG_POS current;
-	int i = current_root->position[X];
-	int j = current_root->position[Y];
 
-	for (int x = max(0, i - 1); x <= min(i + 1, img->rows - 1); x++) {
-		for (int y = max(0, j - 1); y <= min(j + 1, img->cols - 1); y++) {
-			if (x != i || y != j) {
-				current[X] = x;
-				current[Y] = y;
+	int iarr[8] = { -1, -1, 0, +1, +1, +1, 0, -1 };
+	int jarr[8] = { 0, +1, +1, +1, 0, -1, -1, -1 };
+	int x, y;
 
-				if (checkIfTresholdOk(img, start,
-						current, threshold) && !pixels_flags[x][y]) {
-					pixels_flags[x][y] = true;
-					insertDataToEndList(&(current_root->nextPossiblePositions), createTNode(current));
-				}
+	for (int i = 0; i < 8; i++) {
+		x = current_root->position[X] + iarr[i];
+		y = current_root->position[Y] + jarr[i];
+
+		if (isInRange(x, y, img)) {
+			current[X] = x;
+			current[Y] = y;
+			if (checkIfTresholdOk(img, start,
+					current, threshold) && !pixels_flags[x][y]) {
+				pixels_flags[x][y] = true;
+				insertDataToEndList(&(current_root->nextPossiblePositions), createTNode(current));
 			}
 		}
+
+
 	}
+	//for (int x = max(0, i - 1); x <= min(i + 1, img->rows - 1); x++) {
+	//	for (int y = max(0, j - 1); y <= min(j + 1, img->cols - 1); y++) {
+	//		if (x != i || y != j) {
+	//			current[X] = x;
+	//			current[Y] = y;
+
+	//			if (checkIfTresholdOk(img, start,
+	//					current, threshold) && !pixels_flags[x][y]) {
+	//				pixels_flags[x][y] = true;
+	//				insertDataToEndList(&(current_root->nextPossiblePositions), createTNode(current));
+	//			}
+	//		}
+	//	}
+	//}
 }
 
 void findSingleSegmentHelper(GRAY_IMAGE* img, IMG_POS start,
