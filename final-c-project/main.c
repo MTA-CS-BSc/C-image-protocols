918da212e6eff0903f48c84e5a768c24eace2a01
Maya Raskin <mayaraskin14@gmail.com>
2022-12-27 14:25:28 +0200

    Added main to check Q4

--- final-c-project/main.c
+++ final-c-project/main.c
@@ -1,11 +1,14 @@
 #include "Q1.h"
 #include "Q2.h"
 #include "Q3.h"
+#include "Q4.h"
 
 void main() {
 	GRAY_IMAGE* iimg;
 	IMG_POS start = { 3, 15 };
+	IMG_POS_LIST** list = NULL;
+
 	iimg = readPGM("pgm.txt");
-	printGrayMatrix(iimg->pixels, iimg->rows, iimg->cols);
-	findSingleSegment(iimg, start, 10);
+	//printGrayMatrix(iimg->pixels, iimg->rows, iimg->cols);
+	findAllSegments(iimg, 10, list);
 }
