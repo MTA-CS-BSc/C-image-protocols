b245943bfd876a233d2aca5349b6b7edcc65793b
Maya Raskin <mayaraskin14@gmail.com>
2022-12-27 15:45:39 +0200

    Added template for Q5

---
+++ final-c-project/Q5.c
@@ -0,0 +1,6 @@
+#include "Q5.h"
+
+GRAY_IMAGE* colorSegments(IMG_POS_LIST* segments,
+	unsigned int size) {
+
+}
---
+++ final-c-project/Q5.h
@@ -0,0 +1,8 @@
+#ifndef __Q5_H
+#define __Q5_H
+#include "utils.h"
+
+GRAY_IMAGE* colorSegments(IMG_POS_LIST* segments,
+	unsigned int size);
+
+#endif
