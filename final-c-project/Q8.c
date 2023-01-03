bcdc759e3ac0409507e80bda5622d3be084755be
Maya Raskin <mayaraskin14@gmail.com>
2023-01-03 17:17:37 +0200

    Changed name of variables on Q8

--- final-c-project/Q8.c
+++ final-c-project/Q8.c
@@ -14,30 +14,30 @@ char* createPgmFileName(char* fname)
 
 void convertPPMToPGM(char* fname) {
 	int rows, cols, depth, gray_level;
-	FILE *ppmFp = fopen(fname, "r");
-	FILE *pgmFp = fopen(createPgmFileName(fname), "w");
+	FILE *ppm_fp = fopen(fname, "r");
+	FILE *pgm_fp = fopen(createPgmFileName(fname), "w");
 	COLOR_IMAGE* colorFile = readPPM(fname);
 
-	readHeaderFromPicFile(ppmFp, &rows, &cols, &depth);
+	readHeaderFromPicFile(ppm_fp, &rows, &cols, &depth);
 	
-	fprintf(pgmFp, "P3\n%d %d\n%d\n", rows, cols, depth);
+	fprintf(pgm_fp, "P3\n%d %d\n%d\n", rows, cols, depth);
 
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			gray_level = (colorFile->pixels[i][j].r + colorFile->pixels[i][j].g + colorFile->pixels[i][j].b) / 3;
 
-			fprintf(pgmFp, "%d", gray_level);
+			fprintf(pgm_fp, "%d", gray_level);
 
 			if (j == cols - 1)
-				fputc('\n', pgmFp);
+				fputc('\n', pgm_fp);
 
 			else
-				fputc(' ', pgmFp);
+				fputc(' ', pgm_fp);
 			
 
 		}
 	}
 
-	fclose(ppmFp);
-	fclose(pgmFp);
+	fclose(ppm_fp);
+	fclose(pgm_fp);
 }
