3563bef5eab3efc2122656d1bff237a80fbc368c
Maya Raskin <mayaraskin14@gmail.com>
2023-01-05 22:50:52 +0200

    Finished implementing Q11

--- final-c-project/main.c
+++ final-c-project/main.c
@@ -1,6 +1,9 @@
-#include "Q10.h"
+#include "Q11.h"
 
 void main() {
-	convertPPMToPGMBinary("example4p6.ppm");
+	/*GRAY_IMAGE* gi = readBinaryPGM("circle.pgm");
+	printGrayMatrix(gi->pixels, gi->rows, gi->cols);*/
 	/*printRGBMatrix(mat->pixels, mat->rows, mat->cols);*/
+
+	convertPGMToBW_Bin("circle.pgm");
 }
