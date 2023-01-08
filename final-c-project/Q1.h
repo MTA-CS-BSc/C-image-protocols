10d87aac9c462df5182c5e08810fa348acbe3b2e
Maya Raskin <mayaraskin14@gmail.com>
2023-01-08 14:40:37 +0200

    Added comments for Q2

--- final-c-project/Q1.h
+++ final-c-project/Q1.h
@@ -3,7 +3,7 @@
 #include "utils.h"
 
 // Receives a file ref, a color image ref and a flag to determine whether the file is a binary file or a text file. 
-// The function reads the data (the pixels) from the file and inserts them into color image.
+// The function reads the data (the pixels) from the file and inserts them into color image pixels data.
 void readDataFromPPM(FILE* fp, COLOR_IMAGE* color_image, bool is_ascii);
 
 // Receives a valid P3/P6 PPM file, and a flag to determine whether the file is a binary file or a text file.
