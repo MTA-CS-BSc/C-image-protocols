dc60e1f13b5773eb50f48fe42f6d1fa816e1269c
Maya Raskin <mayaraskin14@gmail.com>
2023-01-08 14:41:57 +0200

    Deleted redundant macro

--- final-c-project/Q3.h
+++ final-c-project/Q3.h
@@ -1,7 +1,6 @@
 #ifndef __Q3_H
 #define __Q3_H
 #include "utils.h"
-#define MAX_NEIGHBORS 8
 
 void findRelevantNeighbors(GRAY_IMAGE* img, TNODE* current_root, IMG_POS start,
 	int threshold, bool** pixels_flags);
